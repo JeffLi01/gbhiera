35659d6551c1f579f017a8edac9d2165d49a349e
Jeff Li <lijinfeng01@inspur.com>
2023-06-03 10:38:53 +0800

    feat: show binary data
    Signed-off-by: Jeff Li <lijinfeng01@inspur.com>

--- gbhiera/src/gbhiera.rs
+++ gbhiera/src/gbhiera.rs
@@ -1,8 +1,11 @@
+use std::collections::HashMap;
 use std::path::{PathBuf, Path};
 use std::fs::File;
 use std::io::Read;
 
+
 use rfd;
+use tokio::sync::oneshot::Sender;
 use super::GbhieraUI;
 use futures::future::FutureExt;
 use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
@@ -12,6 +15,10 @@ use slint::ComponentHandle;
 pub enum GbhieraMessage {
     Quit,
     ShowOpenDialog,
+    Expose {
+        line: i32,
+        sender: Sender<String>,
+    },
 }
 
 pub struct GbhieraWorker {
@@ -78,6 +85,15 @@ async fn gbhiera_worker_loop(
                 binary = show_open_dialog();
                 read_binary_future.set(read_binary(binary.clone(), handle.clone()).fuse());
             }
+            GbhieraMessage::Expose{line, sender} => {
+                if let Some(binary_data) = &mut binary_data {
+                    if let Some(s) = binary_data.get_line(line) {
+                        let _ = sender.send(s);
+                        continue;
+                    }
+                }
+                let _ = sender.send("".into());
+            }
         }
     }
 }
@@ -99,19 +115,16 @@ impl From<PathBuf> for Binary {
 }
 
 impl Binary {
-    // fn directory(&self) -> Option<&Path> {
-    //     self.0.parent().filter(|path| path.is_dir())
-    // }
-
     fn to_path(&self) -> &Path {
         &self.0
     }
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default)]
 #[allow(dead_code)]
 struct BinaryData {
     bytes: Vec<u8>,
+    lines: HashMap<i32, String>,
 }
 
 impl BinaryData {
@@ -119,7 +132,58 @@ impl BinaryData {
         let mut f = File::open(binary.to_path())?;
         let mut bytes = vec![];
         f.read_to_end(&mut bytes)?;
-        Ok(BinaryData{ bytes })
+        Ok(BinaryData{
+            bytes,
+            ..Default::default()
+        })
+    }
+
+    pub fn len(&self) -> usize {
+        self.bytes.len()
+    }
+
+    pub fn get_line(&mut self, line: i32) -> Option<String> {
+        if let Some(s) = self.lines.get(&line) {
+            return Some(s.to_owned());
+        }
+        let start = line as usize * 16;
+        if start >= self.len() {
+            return None;
+        }
+
+        let mut end = line as usize * 16 + 16;
+        if end > self.len() {
+            end = self.len();
+        }
+        let chunk = self.bytes.get(start..end);
+        match chunk {
+            Some(bytes) => {
+                let mut s = format!("{:08X} ", line * 16);
+                for i in 0..bytes.len() {
+                    if i < 8 {
+                        s.push_str(&format!("{:02X} ", bytes[i]));
+                    } else {
+                        s.push_str(&format!(" {:02X}", bytes[i]));
+                    }
+                }
+                s.push_str(&format!("{:width$}", "", width = (16 - bytes.len()) * 3));
+                s.push_str("  ");
+                for b in bytes {
+                    let c = match char::from_u32(*b as u32) {
+                        Some(c) => {
+                            if c.is_ascii_graphic() { c } else { '.' }
+                        }
+                        None => '.'
+                    };
+                    s.push(c);
+                }
+                self.lines.insert(line, s.clone());
+                Some(s)
+            }
+            None => {
+                None
+            }
+        }
     }
 }
 
@@ -146,7 +210,7 @@ async fn read_binary(binary: Option<Binary>, handle: slint::Weak<GbhieraUI>) -> 
             handle
                 .clone()
                 .upgrade_in_event_loop(move |h| {
-                    h.set_status("Loading loaded".into());
+                    h.set_status("Binary loaded".into());
                 })
                 .unwrap();
 
@@ -163,13 +227,12 @@ async fn read_binary(binary: Option<Binary>, handle: slint::Weak<GbhieraUI>) -> 
     }
 }
 
-fn apply_binary_data(
-    binary_data: &BinaryData,
-    handle: slint::Weak<GbhieraUI>,
-) {
+fn apply_binary_data(binary_data: &BinaryData, handle: slint::Weak<GbhieraUI>) {
+    let total_line_count = ((binary_data.len() + 15) / 16) as i32;
     handle
         .clone()
         .upgrade_in_event_loop(move |h| {
+            h.set_total_line_count(total_line_count);
         })
         .unwrap();
 }
--- gbhiera/src/main.rs
+++ gbhiera/src/main.rs
@@ -3,17 +3,35 @@ slint::include_modules!();
 mod gbhiera;
 
 use gbhiera::GbhieraMessage;
+use tokio::sync::oneshot;
+use tokio::{self, runtime::Runtime};
 
 fn main() {
-    let mainview = GbhieraUI::new().unwrap();
+    let gbhiera_ui = GbhieraUI::new().unwrap();
 
-    let gbhiera_worker = gbhiera::GbhieraWorker::new(&mainview);
+    let gbhiera_worker = gbhiera::GbhieraWorker::new(&gbhiera_ui);
 
-    mainview.on_show_open_dialog({
+    gbhiera_ui.on_show_open_dialog({
         let cargo_channel = gbhiera_worker.channel.clone();
         move || cargo_channel.send(GbhieraMessage::ShowOpenDialog).unwrap()
     });
+    gbhiera_ui.on_get_line({
+        let cargo_channel = gbhiera_worker.channel.clone();
+        move |line| {
+            let rt = Runtime::new().unwrap();
+            rt.block_on(async {
+                let (sender, receiver) = oneshot::channel::<String>();
+                cargo_channel
+                    .send(GbhieraMessage::Expose {line, sender})
+                    .unwrap();
+                match receiver.await {
+                    Ok(s) => s.into(),
+                    Err(_) => "".into(),
+                }
+            })
+        }
+    });
 
-    mainview.run().unwrap();
+    gbhiera_ui.run().unwrap();
     gbhiera_worker.join().unwrap();
 }
